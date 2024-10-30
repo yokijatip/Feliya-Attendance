e69386e47c7aaee87e87c2998a3338701b602c9a
Yoki Jati Perkasa <yokijati@gmail.com>
2024-10-30 16:15:01 +0700

    save

---
+++ app/src/main/java/com/gity/feliyaattendance/admin/ui/main/detail/worker/AdminWorkerDetailActivity.kt
@@ -0,0 +1,26 @@
+package com.gity.feliyaattendance.admin.ui.main.detail.worker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.gity.feliyaattendance.R
+import com.gity.feliyaattendance.databinding.ActivityAdminWorkerDetailBinding
+
+class AdminWorkerDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdminWorkerDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityAdminWorkerDetailBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
